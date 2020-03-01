fd5ffeb23dc133a039ef70c6cfd76b0876806f1a
jinwanjung <jinwanjung@localhost>
2020-03-01 17:56:33 +0900

    세번째 푸쉬

--- testGit/src/testGit/HelloWorld.java
+++ testGit/src/testGit/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World. 안녕 세계.");
+		System.out.println("Hello World. 안녕 세계");
 
 	}
 
