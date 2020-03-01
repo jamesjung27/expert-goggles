19ecaeb9cfef6f57ec5e12b98398f81c3ce70471
jinwanjung <jinwanjung@172.30.1.40>
2020-03-01 17:34:27 +0900

    First Push 첫 커

---
+++ testGit/src/testGit/HelloWorld.java
@@ -0,0 +1,12 @@
+package testGit;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World. 안녕 세계.");
+
+	}
+
+}
